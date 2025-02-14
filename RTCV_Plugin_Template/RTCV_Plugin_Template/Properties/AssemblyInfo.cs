7165e9788669d224e50a80b2fcfeb9fa4f460a97
Gooberton <129799245+Promethium0@users.noreply.github.com>
2025-02-14 18:41:10 -0500

    Update AssemblyInfo.cs

--- RTCV_Plugin_Template/RTCV_Plugin_Template/Properties/AssemblyInfo.cs
+++ RTCV_Plugin_Template/RTCV_Plugin_Template/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RTCV_PluginTemplate")]
+[assembly: AssemblyTitle("RTCV_PatternEngine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RTCV_PluginTemplate")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("RTCV_PatternEngine")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
